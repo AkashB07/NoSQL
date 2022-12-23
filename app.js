e671bc098dd8a948ae0a71936f2753bf5f73832f
AkashB07 <akashbadhi7@gmail.com>
2022-12-23 15:44:10 +0530

    User Table

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -21,14 +21,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('63a44325a7e0893adc2b77f6')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('63a57947f55bf51d2f68e99d')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -40,6 +40,18 @@ mongoose
   process.env.DB_DETAILS
 )
 .then(result => {
+  User.findOne().then(user => {
+    if(!user){
+      const user = new User({
+        name: 'Akash',
+        email: 'akash@gmail.com',
+        cart: {
+          items: []
+        }
+      });
+      user.save();
+    }
+  });
   app.listen(3000);
 })
 .catch(error => {
